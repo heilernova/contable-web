dd53bfeaae98f0a5d702e8c8cc77db408d1b1b29
Heiler Nova <heilernova@outlook.com>
2024-09-27 18:13:53 -0500

    Add: token en la cabecera de las peticiones http

--- src/app/api/api.interceptor.ts
+++ src/app/api/api.interceptor.ts
@@ -1,7 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { inject } from '@angular/core';
+import { SessionService } from '@app/authentication';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const session = inject(SessionService);
   let url: string = `${environment.API_URL_BASE}/${req.url}`;
-  return next(req.clone({ url }));
+  let headers = req.headers;
+  if (session.session){
+    headers = headers.append('authorization', `bearer ${session.session.token}`);
+  }
+  return next(req.clone({ url, headers }));
 };
